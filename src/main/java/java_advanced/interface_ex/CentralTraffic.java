f82b0a09b2525ecaac20c75b1acbccb44a730d56
ardra-ashok <ardraasok@yahoo.in>
2024-10-14 16:21:24 +1100

    Added Interfaces  example in java

---
+++ src/main/java/java_advanced/interface_ex/CentralTraffic.java
@@ -0,0 +1,9 @@
+package java_advanced.interface_ex;
+
+public interface CentralTraffic {
+    int a = 4;  // public
+    public void greenGo();
+    public void redStop();
+    public void flashYellow();
+
+}
---
+++ src/main/java/java_advanced/interface_ex/ContinentalTraffic.java
@@ -0,0 +1,6 @@
+package java_advanced.interface_ex;
+
+public interface ContinentalTraffic {
+
+    public void TrainSymbol();
+}
